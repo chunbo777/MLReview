25850723384f09a99e3b7c02210fbe96c4e1631f
chunbo777 <apppl2cation@naver.com>
2021-11-29 23:45:12 +0900

    m

---
+++ chapter_3/logit.py
@@ -0,0 +1,138 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn import datasets
+import numpy as np
+import sys
+# sys.path.insert(0, "/Users/seojiwon/Desktop/codingtest/MLReview")
+# from chapter_2.iris import plot_decision_regions
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y)
+
+print('클래스 레이블:', np.unique(y))
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+class LogisticRegressionGD(object):
+    """경사 하강법을 사용한 로지스틱 회귀 분류기
+
+    매개변수
+    ------------
+    eta : float
+      학습률 (0.0과 1.0 사이)
+    n_iter : int
+      훈련 데이터셋 반복 횟수
+    random_state : int
+      가중치 무작위 초기화를 위한 난수 생성기 시드
+
+    속성
+    -----------
+    w_ : 1d-array
+      학습된 가중치
+    cost_ : list
+      에포크마다 누적된 로지스틱 비용 함수 값
+
+    """
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """훈련 데이터 학습
+
+        매개변수
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          n_samples 개의 샘플과 n_features 개의 특성으로 이루어진 훈련 데이터
+        y : array-like, shape = [n_samples]
+          타깃값
+
+        반환값
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            
+            # 오차 제곱합 대신 로지스틱 비용을 계산합니다.
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
+            self.cost_.append(cost)
+        return self
+    
+    def net_input(self, X):
+        """최종 입력 계산"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, z):
+        """로지스틱 시그모이드 활성화 계산"""
+        # 대신 from scipy.special import expit; expit(z) 을 사용할 수 있습니다.
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """단위 계단 함수를 사용하여 클래스 레이블을 반환합니다"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # 다음과 동일합니다.
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+def plot_decision_regions(X, y, classifier, resolution =  0.02):
+
+    markers = ("s", "x", "o", "^", "v")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    x1_min, x1_max = X[:, 0].min()-1, X[:, 0].max()+1
+    x2_min, x2_max = X[:, 1].min()-1, X[:, 1].max()+1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                            np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha = 0.3, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha = 0.8,
+                    c = colors[idx],
+                    marker = markers[idx],
+                    label = cl, 
+                    edgecolor="black")
+
+X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
+y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+
+lrgd = LogisticRegressionGD(eta=0.05, n_iter=1000, random_state=1)
+lrgd.fit(X_train_01_subset,
+         y_train_01_subset)
+
+plot_decision_regions(X=X_train_01_subset, 
+                      y=y_train_01_subset,
+                      classifier=lrgd)
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+# plt.savefig('images/03_05.png', dpi=300)
+plt.show()
